582e8dad1f6ef8e731610085fe53db51ba30c9d9
Faith Awuor <faithhope264@gmail.com>
2025-09-17 16:39:35 +0300

    Initial commit of a new project

---
+++ basic_operations.py
@@ -0,0 +1,12 @@
+number1=10
+number2=5
+addition= number1+number2
+print("addition of numbers:",addition)
+number1=10
+number2=5
+subtraction= number1-number2
+print("subtraction of numbers:",subtraction)
+number1=10
+number2=5
+multiplication= number1 * number2
+print("multiplication of numbers:",multiplication)
