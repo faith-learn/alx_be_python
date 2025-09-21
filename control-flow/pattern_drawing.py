0b01ec08a03dc1b42814699fe896a055237bf767
Faith Awuor <faithhope264@gmail.com>
2025-09-21 12:49:37 +0300

    Initial changes for the new project

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,6 +1,8 @@
 size = int(input("Enter the size of the pattern: "))
 
-for i in range(size):
-    for j in range(size):
-        print("* ", end="")
+row = 0
+while row < size:
+    for col in range(size):
+        print("*", end="")
     print()
+    row += 1
