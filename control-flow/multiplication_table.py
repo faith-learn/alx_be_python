6433cfb113b36ba2444c9e6f1591dc05b65de7ee
Faith Awuor <faithhope264@gmail.com>
2025-09-21 12:34:10 +0300

    Initial changes of task 1

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,7 +1,11 @@
 number=int(input("Enter a number to see its multiplication table:"))
 for i in range(1,11):
     #print a header for each table
-    print(f"multiplication table for {i}:")
+    number = int(input("Enter a number to see its multiplication table: "))
+
+for i in range(1, 11):
+    result = number * i
+    print(f"{number} * {i} = {result}")
     #use an inner for loop for the multipliers(1,10)
     for j in range(1,11):
         #print the product for each iteration
